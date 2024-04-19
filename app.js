3e0f82289906c989cafaae63f7632c2aa22236eb
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-04-18 20:59:18 -0600

    added the auto fill zipCode and random generat park description

---
+++ app.js
@@ -0,0 +1,90 @@
+"use strict";
+
+const smartyUrl = "";
+
+const parksUrl = "";
+
+const addressField = document.querySelector("#address");
+const cityField = document.querySelector("#city");
+const stateField = document.querySelector("#state");
+const zipField = document.querySelector("#zip");
+const parkThumb = document.querySelector("#specials h2 img");
+const parkSection = document.querySelector("#specials");
+const parkName = document.querySelector("#specials h2 a");
+const parkDesc = document.querySelector("#specials p");
+
+const smartyUpdateUISuccess = function (data) {
+  const parsedData = JSON.parse(data);
+  //  console.log(parsedData);
+  const zip = parsedData[0].components.zipcode;
+  const plus4 = parsedData[0].components.plus4_code;
+  //  console.log(zip + '-' + plus4);
+  zipField.value = zip + "-" + plus4;
+};
+const parkUpdateUISuccess = function (parsedData) {
+  // const parsedData = JSON.parse(data);
+  console.log(parsedData);
+  const number = Math.floor(Math.random() * parsedData.data.length);
+  parkName.textContent = parsedData.data[number].fullName;
+  parkName.href = parsedData.data[number].url;
+  parkDesc.textContent = parsedData.data[number].description;
+  parkThumb.src =
+    "https://www.nps.gov/theme/assets/dist/images/branding/logo.png";
+  parkSection.classList.remove("hidden");
+};
+const smartyUpdateUIError = function (error) {
+  console.log(error);
+};
+const parkUpdateUIError = function (error) {
+  console.log(error);
+};
+
+// const responseMethod = function(httpRequest, succeed, fail) {
+//   if (httpRequest.readyState === 4) {
+//     if (httpRequest.status === 200) {
+//       succeed(httpRequest.responseText);
+//     } else {
+//       fail(httpRequest.status + ': ' + httpRequest.responseText);
+//     }
+//   }
+// }
+
+// const createRequest = function(url, succeed, fail) {
+//   const httpRequest = new XMLHttpRequest(url);
+//   httpRequest.addEventListener('readystatechange', (url) => responseMethod(httpRequest, succeed, fail));
+//   httpRequest.open('GET', url);
+//   httpRequest.send();
+// };
+
+const createRequest = function (url, succeed, fail) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => succeed(data));
+};
+
+const checkCompletion = function () {
+  if (
+    addressField.value !== "" &&
+    cityField.value !== "" &&
+    stateField.value !== ""
+  ) {
+    const requestUrl =
+      smartyUrl +
+      "&street=" +
+      addressField.value +
+      "&city=" +
+      cityField.value +
+      "&state=" +
+      stateField.value;
+    createRequest(requestUrl, smartyUpdateUISuccess, smartyUpdateUIError);
+  }
+};
+//createRequest(smartyUrl);
+//createRequest(parksUrl, parkUpdateUISuccess, parkUpdateUIError);
+
+addressField.addEventListener("blur", checkCompletion);
+cityField.addEventListener("blur", checkCompletion);
+stateField.addEventListener("blur", checkCompletion);
+window.addEventListener("DOMContentLoaded", () => {
+  createRequest(parksUrl, parkUpdateUISuccess, parkUpdateUIError);
+});
